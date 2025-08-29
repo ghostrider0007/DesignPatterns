96fb0aa268a26eed3dd90eb8ad59e6461e8a06a1
Raj Vishnu <80027368+ghostrider0007@users.noreply.github.com>
2025-08-29 23:56:58 +0530

    testing (#1)
    Co-authored-by: rajv-ideaforge <raj.vishnu@ideaforgetech.com>

--- src/FactoryPattern/Main.java
+++ src/FactoryPattern/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         ShapeFactory factory = new ShapeFactory();
 
-        Shape obj1 = factory.getShape("Square");
+        Shape shape = factory.getShape("Square");
 
-        obj1.draw();
+        shape.draw();
 
     }
 }
