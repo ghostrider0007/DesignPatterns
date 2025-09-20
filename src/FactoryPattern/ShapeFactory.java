dd77f87d4b82b37bc4ed53e9067bb9247f4ab8ec
rajv-ideaforge <raj.vishnu@ideaforgetech.com>
2025-09-20 15:27:47 +0530

    testing1

--- src/FactoryPattern/ShapeFactory.java
+++ src/FactoryPattern/ShapeFactory.java
@@ -10,7 +10,7 @@ public class ShapeFactory {
             case "Square":
                 return new Square();
             default:
-                return null; // testing
+                return null; // testing1
         }
     }
 }
